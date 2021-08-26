f23f0f8e75a5b17914082301ee0a0dc9fbb912f1
Mert <42304219+mertcihangiroglu@users.noreply.github.com>
2021-08-26 12:47:30 +0200

    Update extract_frames.py

--- extract_frames.py
+++ extract_frames.py
@@ -8,7 +8,7 @@ while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == False:
         break
-    if count% 4 == 0:            # I am extracting 1 frame in every 4 frames you can change this.
+    if count% 4 == 0:            # I am extracting 1 frame in every 4 frames you can change this. !!!!!make 0 indices part fixed !!
         cv2.imwrite('frame_'+str(i)+'.jpg',frame)
         count+=1
         i+=1
